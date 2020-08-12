5b0b0dd5b7bda83a5a419614d7da92d7606a634c
Beekasha <brian.j.kleiner@gmail.com>
2020-08-12 13:28:28 -0400

    deleting bloat code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1> Testing </h1>
-      </header>
+      <h1> Hey </h1>
     </div>
   );
 }
